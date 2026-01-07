f280b4d2a8d39d55ebe71750c094acaa4b055103
Sahil Singh <sahilsinghchib33534@gmail.com>
2026-01-07 12:01:06 +0530

    feat(terraform) : add vnet and subnetnetworking

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,21 @@ resource "azurerm_resource_group" "rg" {
 	name = "rg-terraform-lab"
 	location = "eastus"
 }
+
+
+resource "azurerm_virtual_network" "vnet" {
+name = "vnet-terraform-lab" 
+address_space = ["10.0.0.0/16"]  
+location = azurerm_resource_group.rg.location 
+resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_subnet" "subnet" {
+
+name = "subnet-terraform-lab"
+resource_group_name = azurerm_resource_group.rg.name
+virtual_network_name = azurerm_virtual_network.vnet.name
+address_prefixes =["10.0.1.0/24"]
+
+}
